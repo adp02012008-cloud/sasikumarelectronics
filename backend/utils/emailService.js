927d17e55025eaea1c569ff9ae051add44dc3451
DHASHAPRAKASH <adp02012008@gmail.com>
2026-06-14 23:46:38 +0530

    Fix Brevo email API service

--- backend/utils/emailService.js
+++ backend/utils/emailService.js
@@ -1,32 +1,111 @@
-const axios = require("axios");
+const axios =
+require("axios");
 
-const sendEmail = async (options) => {
-  const response = await axios.post(
-    "https://api.brevo.com/v3/smtp/email",
-    {
-      sender: {
-        name: "Sasikumar Electronics",
-        email: process.env.EMAIL_FROM,
-      },
-      to: [
-        {
-          email: options.to,
-        },
-      ],
-      subject: options.subject,
-      htmlContent: options.html,
+
+const sendEmail =
+async(options)=>{
+
+
+ try{
+
+
+  const response =
+  await axios.post(
+
+   "https://api.brevo.com/v3/smtp/email",
+
+
+   {
+
+
+    sender:{
+
+     name:
+     "Sasikumar Electronics",
+
+     email:
+     process.env.EMAIL_FROM
+
     },
-    {
-      headers: {
-        accept: "application/json",
-        "api-key": process.env.BREVO_API_KEY,
-        "content-type": "application/json",
-      },
+
+
+    to:[
+
+     {
+
+      email:
+      options.to
+
+     }
+
+    ],
+
+
+    subject:
+    options.subject,
+
+
+    htmlContent:
+    options.html
+
+
+   },
+
+
+   {
+
+
+    headers:{
+
+
+     "api-key":
+     process.env.BREVO_API_KEY,
+
+
+     "Content-Type":
+     "application/json"
+
+
     }
+
+
+   }
+
+
   );
 
-  console.log("Email Sent:", response.data);
+
+  console.log(
+   "Email Sent Successfully"
+  );
+
+
   return response.data;
+
+
+ }
+
+
+ catch(error){
+
+
+  console.log(
+
+   "BREVO ERROR:",
+
+   error.response?.data
+   ||
+   error.message
+
+  );
+
+
+ }
+
+
 };
 
-module.exports = sendEmail;
+
+
+module.exports =
+sendEmail;
